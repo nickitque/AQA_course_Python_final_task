57e04833d80a65b37362846982e8718552120fec
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-19 20:46:42 +0300

    Edited fixture in conftest. Added test_items.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,27 @@
+import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    """Work with terminal for choosing language. The defaul lang is fr."""
+    parser.addoption("--language", action="store", default="fr",
+                     help="Choose language: ru or es or other")
+
+
+"""Fixture for choosing lang."""
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    """Work with language in headers."""
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
---
+++ test_items.py
@@ -0,0 +1,16 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_find_basket_button(browser):
+    browser.get(url)
+    basket_button = browser.find_elements_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
+    """
+    !!!!!!! the examiner has himself add time.sleep !!!!!!!
+    """
+    #time.sleep(15)
+    assert basket_button, "There is no element of basket button"
