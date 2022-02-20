3dae3fda88c7a508d1440cba10a6e732d4ee2069
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-20 19:59:25 +0300

    Added locators and updated main page tests

--- pages/locators.py
+++ pages/locators.py
@@ -5,7 +5,6 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     CART_LINK = (By.XPATH, "/html/body/header/div[1]/div/div[2]/span/a")
-    CART_MESSAGE_EMPTY = (By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/p")
 
 class MainPageLocators():
     LOGIN_LINK = (By.ID, "login_link")
@@ -18,3 +17,6 @@ class ProductPageLocators():
     ADD_TO_CART = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
     SUCCESS_MESSAGE = (By.XPATH, "/html/body/div[2]/div/div[1]/div[1]/div")
     ALERT_INNER_DISCOUNT = (By.XPATH, "/html/body/div[2]/div/div[1]/div[2]/div")
+
+class BasketPageLocators():
+    CART_MESSAGE_EMPTY = (By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/p")
--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,27 @@
 import time
+import pytest
 
 from pages.main_page import MainPage
 
-
 link = "http://selenium1py.pythonanywhere.com"
-#http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    login_page = page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
-    login_page.should_be_login_page()
+# http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(browser):
+        page = MainPage(browser,
+                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        login_page = page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     """Гость открывает главную страницу. Переходит в корзину по кнопке в шапке сайта
