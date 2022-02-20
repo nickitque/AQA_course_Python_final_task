9a1eea23ee3b629fd32ed0944a3ce324ef38bfb0
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-20 18:30:12 +0300

    Added new methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,8 +11,16 @@ class ProductPage(BasePage):
         button_add_to_cart.click()
 
     def check_messages(self):
-        """Метод, который будет проверять наличие сообщений о добавлении в корзину."""
-        alert_message_success = (self.browser.find_element(*ProductPageLocators.ALERT_INNER_CART)).text
+        """Метод, который будет проверять наличие сообщений о добавлении в корзину и действующей скидке."""
+        alert_message_success = (self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)).text
         alert_message_discount = (self.browser.find_element(*ProductPageLocators.ALERT_INNER_DISCOUNT)).text
         assert "был добавлен в вашу корзину" in alert_message_success
         assert "Ваша корзина удовлетворяет условиям предложения" in alert_message_discount
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def is_element_dissapeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
+
