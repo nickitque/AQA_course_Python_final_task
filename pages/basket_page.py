9dfac67f9a4d8abbc2c9d1c8c8dabac92abcf002
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-20 19:33:42 +0300

    Creating Basket page file

---
+++ pages/basket_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def check_message_in_cart(self):
+        empty_cart_message = (self.browser.find_element(*BasketPageLocators.CART_MESSAGE_EMPTY)).text
+        assert "Ваша корзина пуста" in empty_cart_message
