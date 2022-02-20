295121e7885b4e75b76d4cfc5af40679a8de6173
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-20 12:58:09 +0300

    Edite main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
         """Метод, который будет проверять наличие ссылки"""
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
